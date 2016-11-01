666c432ebf23ea4a63afff89f57a6b00501a956a
NazgulLee <lijian1994@hotmail.com>
2016-11-01 15:41:56 +0800

    first commit

---
+++ 数字版权交易系统/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  数字版权交易系统
+//
+//  Created by 李剑 on 16/11/1.
+//  Copyright © 2016年 zdrjxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 数字版权交易系统/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  数字版权交易系统
+//
+//  Created by 李剑 on 16/11/1.
+//  Copyright © 2016年 zdrjxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
